ae510e0d5d950b80aa2f001ecfc280e47aaa1569
Mathew Robinson <mathew.robinson3114@gmail.com>
2014-11-30 12:57:12 -0500

    Fixed the pubipwatchdog.py script now grabs command output

--- pubipwatchdog.py
+++ pubipwatchdog.py
@@ -1,13 +1,15 @@
 import smtplib
-from subprocess import call
+import subprocess
 
 # This will check if our public IP changed by comparing the freshly pulled
 # IP against our pubip file that contains the last pulled IP address
 def checkIfIpChanged(ip):
     ipFile = open("/home/mathew/.pubip", "r")
     if(ipFile.read() == ip):
+        ipFile.close()
         return False
     else:
+        ipFile.close()
         writeNewIPToFile(ip)
         return True
 
@@ -15,6 +17,7 @@ def checkIfIpChanged(ip):
 def writeNewIPToFile(ip):
     ipFile = open("/home/mathew/.pubip", "w")
     ipFile.write(ip)
+    ipFile.close()
 
 # Sends our email with the new IP if it has changed.
 def emailNewIP(ip):
@@ -35,9 +38,11 @@ def emailNewIP(ip):
 
 # This is because I like C get over it.
 def main():
-    publicIP = call(["curl", "icanhazip.com"])
-    if(checkIfIpChanged(string(publicIP)):
-        emailNewIP(string(publicIP))
+    publicIP = subprocess.Popen(["curl", "icanhazip.com"], stdout=subprocess.PIPE).communicate()[0]
+    print("String of PublicIP " + str(publicIP))
+    if(checkIfIpChanged(str(publicIP))):
+        #emailNewIP(str(publicIP))
+        print(publicIP)
     else:
         print("IP didn't change.")
 
