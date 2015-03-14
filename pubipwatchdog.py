d8f3d152ae4e7559350ad8a2e51de8e3a64f6b5d
Mathew Robinson <mathew.robinson3114@gmail.com>
2015-03-14 10:24:17 -0400

    Now takes the email, password, and filepath as arguments with flags. Readme incoming.

--- pubipwatchdog.py
+++ pubipwatchdog.py
@@ -6,42 +6,45 @@ import sys
 
 # This will check if our public IP changed by comparing the freshly pulled
 # IP against our pubip file that contains the last pulled IP address
-def checkIfIpChanged(ip):
-    ipFile = open(sys.argv[1] + ".pubip", "r")
+def checkIfIpChanged(ip, fpath):
+    ipFile = open(fpath + ".pubip", "r")
     if(ipFile.read() == ip):
         ipFile.close()
         return False
     else:
         ipFile.close()
-        writeNewIPToFile(ip)
+        writeNewIPToFile(ip, fpath)
         return True
 
-def doesPubIPfileExist():
-    ipFile = subprocess.Popen(["ls", "-a", sys.argv[1]], stdout=subprocess.PIPE).communicate()[0]
+# This runs everytime the script does to verify the pubip file exists because if
+# it didn't exist and we ran the rest of the script it would crash due to the way
+# Python's open function works.
+def doesPubIPfileExist(fpath):
+    ipFile = subprocess.Popen(["ls", "-a", fpath], stdout=subprocess.PIPE).communicate()[0]
     if".pubip" in ipFile:
         return
     else:
-        ipFile = open(sys.argv[1] + ".pubip", "w")
+        ipFile = open(fpath + ".pubip", "w")
         ipFile.write("0.0.0.0")
         ipFile.close()
         return
 
 # This will write the new IP to the .pubip file if it has changed
-def writeNewIPToFile(ip):
-    ipFile = open(sys.argv[1] + ".pubip", "w")
+def writeNewIPToFile(ip, fpath):
+    ipFile = open(fpath + ".pubip", "w")
     ipFile.write(ip)
     ipFile.close()
 
 # Sends our email with the new IP if it has changed.
-def emailNewIP(ip):
-    fromaddr = 'pubipwatchdog@gmail.com'
+def emailNewIP(ip, uname, passwd):
+    fromaddr = uname
     toaddrs  = 'mathew.robinson3114@gmail.com'
     msg = ip
 
 
     # Credentials (if needed)
-    username = ''
-    password = ''
+    username = uname
+    password = passwd
 
     # The actual mail send
     server = smtplib.SMTP('smtp.gmail.com:587')
@@ -50,13 +53,25 @@ def emailNewIP(ip):
     server.sendmail(fromaddr, toaddrs, msg)
     server.quit()
 
+# Interperets the command line args we get and returns the usable values
+def scanCmdArguments():
+    for index, arg in enumerate(sys.argv):
+        if arg == "-p":
+            # if we see the -p flag then the next element will be a space so
+            # we go to the next next element.
+            fpath = sys.argv[index + 1]
+        elif arg ==  "-u":
+            uname = sys.argv[index + 1]
+        elif arg == "-ep":
+            passwd = sys.argv[index + 1]
+    return fpath, uname, passwd
+
 # This is because I like C get over it.
 def main():
-    doesPubIPfileExist()
+    fpath, uname, passwd = scanCmdArguments()
+    doesPubIPfileExist(fpath)
     publicIP = subprocess.Popen(["curl", "icanhazip.com"], stdout=subprocess.PIPE).communicate()[0]
-    if(checkIfIpChanged(str(publicIP))):
-        #emailNewIP(str(publicIP))
-        print(publicIP)
-        print("SUccess")
+    if(checkIfIpChanged(str(publicIP), fpath)):
+        emailNewIP(str(publicIP), uname, passwd)
 
 main()
