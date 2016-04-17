20b925c619d049e4201ef158ef4d88a1aef8f1d3
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-04-17 15:32:48 -0400

    Fixed weird issues with unicode and ascii comparing

--- ipnoti.py
+++ ipnoti.py
@@ -1,35 +1,37 @@
 #!/bin/python3
 
 import smtplib
+import requests
 import subprocess
 import sys
 import os
-import urllib.request
 
-homedir = os.getenv("HOME", "")
+homedir = os.getenv("HOME", "") + "/"
 username = os.getenv("IPNOTI_USERNAME", "")
 password = os.getenv("IPNOTI_PASSWORD", "")
 target_email = os.getenv("IPNOTI_TARGET", "")
 
 # This will check if our public IP changed by comparing the freshly pulled
 # IP against our pubip file that contains the last pulled IP address
-def checkIfIpChanged(ip):
+def hasIpChanged(ip):
     with open(homedir + ".pubip", "r+") as ipFile:
-        if(ipFile.read() == ip):
+        if ipFile.read().strip() in ip.strip():
             return False
         else:
+            ipFile.seek(0)
+            ipFile.truncate()
             ipFile.write(ip)
             return True
 
 # Sends our email with the new IP if it has changed.
 def emailNewIP(ip):
-    # The actual mail send
     server = smtplib.SMTP('smtp.gmail.com:587')
     server.starttls()
     server.login(username,password)
     server.sendmail(username, target_email, ip)
     server.quit()
 
-publicIP = urllib.request.urlopen("http://canhazip.com/").read()
-if(checkIfIpChanged(str(publicIP))):
-    emailNewIP(str(publicIP))
+r = requests.get("http://canhazip.com/")
+ip = r.text
+if(hasIpChanged(ip)):
+    emailNewIP(ip)
